f2849c960f16dfa726cb3fbf6ddd69242886666e
muhammad sufyan <m.sufyan94@yahoo.com>
2019-04-22 14:41:19 +0500

    Changeings in email verification

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+Route::get('profile', function () {
+    // Only verified users may enter...
+})->middleware('verified');
+
+Route::get('/home', 'HomeController@index')->name('home');
