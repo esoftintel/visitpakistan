bf531b1d2e0c3ebd7c3ebb9db718956cd56d789f
muhammad sufyan <m.sufyan94@yahoo.com>
2019-05-18 13:30:51 +0500

    18 may

--- app/Http/Controllers/API/auth_api.php
+++ app/Http/Controllers/API/auth_api.php
@@ -20,7 +20,7 @@ class auth_api extends Controller
          */ 
         public function login(){ 
             //print_r(request('password')); exit;
-           // print_r(attempt(['email' => request('email'), 'password' => request('password')])); exit;
+           print_r(Auth::attempt(['email' => request('email'), 'password' => request('password')])); exit;
             if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
                 $user = Auth::user(); 
 
@@ -48,7 +48,7 @@ class auth_api extends Controller
             return response()->json(['error'=>$validator->errors()], 401);            
         }
         $input = $request->all(); 
-        $input['password'] = bcrypt($input['password']); 
+        $input['password'] = bcrypt($input['password']);
         $user = User::create($input); 
         $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
