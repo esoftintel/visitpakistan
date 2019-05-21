9ebb3af2e05888ec923cad964363c67bbc77d3e7
m_zeshan <ciit104@gmail.com>
2019-05-21 14:16:23 +0500

    hfhgfhgfhg

--- app/Http/Controllers/API/auth_api.php
+++ app/Http/Controllers/API/auth_api.php
@@ -18,7 +18,9 @@ class auth_api extends Controller
          * 
          * @return \Illuminate\Http\Response 
          */ 
-        public function login(){ 
+        public function login(Request $request){ 
+
+
             if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
                 $user = Auth::user(); 
                 $success['token'] =  $user->createToken('MyApp')-> accessToken; 
